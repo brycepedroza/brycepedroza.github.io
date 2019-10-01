1f927f4d58674a1178c8cbefccd1eb934417b66c
brycepedroza <bapedroz@asu.edu>
2019-09-30 22:12:08 -0700

    Updates

---
+++ precache-manifest.fd8e02b83246cb33988583ba2aa123b3.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579360f2bf1c9ec8c1d1af705054e82e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afba8f7a67a3deb9ef3f",
+    "url": "/static/css/2.962b12d2.chunk.css"
+  },
+  {
+    "revision": "4f3badcd9d574eabcee3",
+    "url": "/static/css/main.ba2d5075.chunk.css"
+  },
+  {
+    "revision": "afba8f7a67a3deb9ef3f",
+    "url": "/static/js/2.36fb371b.chunk.js"
+  },
+  {
+    "revision": "4f3badcd9d574eabcee3",
+    "url": "/static/js/main.2b27b950.chunk.js"
+  },
+  {
+    "revision": "bd401bf1d39abb077abd",
+    "url": "/static/js/runtime~main.b4b9169f.js"
+  },
+  {
+    "revision": "04a49a03876500ead95b86b33d7355d4",
+    "url": "/static/media/BrycePedrozaFall2019Resume.04a49a03.pdf"
+  },
+  {
+    "revision": "0b65c955657ae2f60d452cdaa22af5e5",
+    "url": "/static/media/BrycePedrozaSpring2019Resume.0b65c955.pdf"
+  },
+  {
+    "revision": "1971b18aa8b542a59f2a5dc1a40df18d",
+    "url": "/static/media/asulogo.1971b18a.png"
+  },
+  {
+    "revision": "34c3707f63bb964fd1d47d17e16a2162",
+    "url": "/static/media/background1.34c3707f.png"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "91c87ebe24ed510e22d1c4bdbb19d8ca",
+    "url": "/static/media/defcon.91c87ebe.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "8ef618cbd5df220006777c2d87b63c12",
+    "url": "/static/media/grandcanyon.8ef618cb.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a1f835e3d5e95d29f63215307ab2d206",
+    "url": "/static/media/italy.a1f835e3.jpg"
+  },
+  {
+    "revision": "013e070d67b95b5bd0da87845f9d5385",
+    "url": "/static/media/mapreducelogo.013e070d.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b2b7b6c09e93f796250c6e7964864f74",
+    "url": "/static/media/reactlogo.b2b7b6c0.jpg"
+  },
+  {
+    "revision": "73576eb90dea6f975a7d51cfbde6ec73",
+    "url": "/static/media/starbuckslogo.73576eb9.png"
+  },
+  {
+    "revision": "0b1d9c2e640356c043ab0662268d61ae",
+    "url": "/static/media/walmartlogo.0b1d9c2e.png"
+  }
+]);
